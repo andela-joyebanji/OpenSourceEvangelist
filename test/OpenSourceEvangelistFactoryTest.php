529be0a83dc1e808a64d2094d4f16f64bd592cec
andela-joyebanji <oyebanji.jacob@andela.com>
2016-02-28 22:38:23 +0100

    Add new tests
    Add new test cases for OpenSourceEvangelistFactory class
    
    Add new test for

--- test/OpenSourceEvangelistFactoryTest.php
+++ test/OpenSourceEvangelistFactoryTest.php
@@ -3,6 +3,9 @@
 use Pyjac\OpenSourceEvangelist\OpenSourceEvangelistFactory;
 use Pyjac\OpenSourceEvangelist\Evangelist\EvangelistAbstract;
 use Pyjac\OpenSourceEvangelist\Evangelist\JuniorEvangelist;
+use Pyjac\OpenSourceEvangelist\Evangelist\AssociateEvangelist;
+use Pyjac\OpenSourceEvangelist\Evangelist\SeniorEvangelist;
+use Pyjac\OpenSourceEvangelist\Evangelist\NoneEvangelist;
 
 class OpenSourceEvangelistFactoryTest extends PHPUnit_Framework_TestCase
 {
@@ -19,10 +22,40 @@ class OpenSourceEvangelistFactoryTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf(EvangelistAbstract::class, $evangelist);
     }
 
-    public function testCreateEvangelistReturnsCorrectEvangelist()
+    public function testCreateEvangelistToReturnJuniorEvangelistWhenNumberOfReposGreaterThan4ButLessThan11IsPassed()
     {
         $evangelist = $this->openSourceEvangelistFactory->createEvangelist('pyjac', 10);
         $this->assertInstanceOf(JuniorEvangelist::class, $evangelist);
+
+        $evangelist = $this->openSourceEvangelistFactory->createEvangelist('pyjac', 5);
+        $this->assertInstanceOf(JuniorEvangelist::class, $evangelist);
+    }
+
+    public function testCreateEvangelistToReturnAssociateEvangelistWhenNumberOfReposGreaterThan10ButLessThan21IsPassed()
+    {
+        $evangelist = $this->openSourceEvangelistFactory->createEvangelist('pyjac', 11);
+        $this->assertInstanceOf(AssociateEvangelist::class, $evangelist);
+
+        $evangelist = $this->openSourceEvangelistFactory->createEvangelist('pyjac', 20);
+        $this->assertInstanceOf(AssociateEvangelist::class, $evangelist);
+    }
+
+    public function testCreateEvangelistToReturnSeniorEvangelistWhenNumberOfReposGreaterThan20IsPassed()
+    {
+        $evangelist = $this->openSourceEvangelistFactory->createEvangelist('pyjac', 21);
+        $this->assertInstanceOf(SeniorEvangelist::class, $evangelist);
+
+        $evangelist = $this->openSourceEvangelistFactory->createEvangelist('pyjac', 100);
+        $this->assertInstanceOf(SeniorEvangelist::class, $evangelist);
+    }
+
+    public function testCreateEvangelistToReturnNoneEvangelistWhenNumberOfReposLessThan5IsPassed()
+    {
+        $evangelist = $this->openSourceEvangelistFactory->createEvangelist('pyjac', 4);
+        $this->assertInstanceOf(NoneEvangelist::class, $evangelist);
+
+        $evangelist = $this->openSourceEvangelistFactory->createEvangelist('pyjac', 0);
+        $this->assertInstanceOf(NoneEvangelist::class, $evangelist);
     }
 
 }
