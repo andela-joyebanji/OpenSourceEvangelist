7589f72cc0498b16d05c44f6c716bfea90b02802
andela-joyebanji <oyebanji.jacob@andela.com>
2016-02-28 23:31:09 +0100

    [Chore] Add check for correct argument type
    The arguments passed into the createEvangelist method must be of type string ($username) and int ($repos) respectively
    for it to function properly.

--- src/OpenSourceEvangelistFactory.php
+++ src/OpenSourceEvangelistFactory.php
@@ -12,16 +12,16 @@ class OpenSourceEvangelistFactory implements OpenSourceEvangelistFactoryInterfac
 
 	public function createEvangelist($username, $repos)
 	{
+		if(!is_string($username) || !is_int($repos)){
+			throw new \InvalidArgumentException("Username must be type of string and number of repository of type interger.");
+		}
 		if($repos >= 5 && $repos <= 10){
-
 			return new JuniorEvangelist($username, $repos);
 		}
 		if($repos >= 11 && $repos <= 20){
-
 			return new  AssociateEvangelist($username, $repos);
 		}
 		if($repos >= 21){
-
 			return new SeniorEvangelist($username, $repos);
 		}
 
