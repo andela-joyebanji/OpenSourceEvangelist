1731cbb430d97077a701a2ca774cd490d790b527
andela-joyebanji <oyebanji.jacob@andela.com>
2016-02-29 11:59:42 +0100

    Fix documentation

--- src/OpenSourceEvangelistDataSource.php
+++ src/OpenSourceEvangelistDataSource.php
@@ -8,6 +8,14 @@ use Pyjac\OpenSourceEvangelist\Exception\OpenSourceEvangelistNotFoundException;
 
 class OpenSourceEvangelistDataSource implements OpenSourceEvangelistDataSourceInterface
 {
+    /**
+     * Get data of the provided username from the data source.
+     *     
+     * @param  string $username 
+     * @throws Pyjac\OpenSourceEvangelist\Exception\OpenSourceEvangelistNotFoundException
+     * 
+     * @return \stdClass          
+     */
     public function getEvangelistData($username)
     {
         try {
