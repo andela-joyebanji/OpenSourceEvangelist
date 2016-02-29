a9969ffd17397f82101e75ede66f4620294f3571
andela-joyebanji <oyebanji.jacob@andela.com>
2016-02-29 22:52:10 +0100

    Add comment

--- src/OpenSourceEvangelistDataSourceInterface.php
+++ src/OpenSourceEvangelistDataSourceInterface.php
@@ -4,5 +4,14 @@ namespace Pyjac\OpenSourceEvangelist;
 
 interface OpenSourceEvangelistDataSourceInterface
 {
+	 /**
+     * Get data of the provided username from the data source.
+     *
+     * @param string $username
+     *
+     * @throws Pyjac\OpenSourceEvangelist\Exception\OpenSourceEvangelistNotFoundException
+     *
+     * @return \stdClass
+     */
     public function getEvangelistData($username);
 }
--- src/OpenSourceEvangelistFactoryInterface.php
+++ src/OpenSourceEvangelistFactoryInterface.php
@@ -4,5 +4,13 @@ namespace Pyjac\OpenSourceEvangelist;
 
 interface OpenSourceEvangelistFactoryInterface
 {
+	/**
+     * Factory method to create an instance of an evangelist.
+     *
+     * @param string $username
+     * @param int    $repos
+     *
+     * @return Pyjac\OpenSourceEvangelist\Evangelist\EvangelistAbstract
+     */
     public function createEvangelist($username, $repos);
 }
