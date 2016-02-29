10450df7fb773ab2ca8f8c6077f650960ec6644f
Oyebanji Jacob Mayowa <oyebanji.jacob@andela.com>
2016-02-29 18:03:06 -0500

    Applied fixes from StyleCI

--- index.php
+++ index.php
@@ -2,16 +2,15 @@
 
 require 'vendor/autoload.php';
 
+use Pyjac\OpenSourceEvangelist\Exception\OpenSourceEvangelistNotFoundException;
 use Pyjac\OpenSourceEvangelist\OpenSourceEvangelist;
 use Pyjac\OpenSourceEvangelist\OpenSourceEvangelistDataSource;
 use Pyjac\OpenSourceEvangelist\OpenSourceEvangelistFactory;
-use Pyjac\OpenSourceEvangelist\Exception\OpenSourceEvangelistNotFoundException;
 
-try
-{
-	$openSourceEvangelist = new OpenSourceEvangelist(new OpenSourceEvangelistDataSource(), new OpenSourceEvangelistFactory());
-	$evangelist = $openSourceEvangelist->getEvangelist('*t');
-	echo $evangelist->getStatus();
-} catch(OpenSourceEvangelistNotFoundException $ex){
-	echo $ex->getMessage();
+try {
+    $openSourceEvangelist = new OpenSourceEvangelist(new OpenSourceEvangelistDataSource(), new OpenSourceEvangelistFactory());
+    $evangelist = $openSourceEvangelist->getEvangelist('*t');
+    echo $evangelist->getStatus();
+} catch (OpenSourceEvangelistNotFoundException $ex) {
+    echo $ex->getMessage();
 }
